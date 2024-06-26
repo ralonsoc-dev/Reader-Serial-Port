0d219c5c99ae56157d9bc225402c8e0b8b0e8c6f
ralonsoc-dev <raul.ac-97@hotmail.com>
2024-06-26 22:45:51 +0200

    Lectura dedos, accelerometro y seleccion letra, hacer media de lecturas y exportar a csv para importar en bd con la estructura: Letra, lectura dedos y lectura accelerometro

--- Ventana.py
+++ Ventana.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+
 import customtkinter as ctk
 import serial.tools.list_ports
 from PIL import Image, ImageTk
@@ -39,6 +41,11 @@ class Ventana(object):
         self.serial_reader = None
         self.hilo_lectura = None
         self.detener_lectura = False
+
+        # Inicializo listas
+        self.mediaDedos = []
+        self.accelerometro = []
+        self.letra = ""
           
     def run(self):
         self.app.mainloop()
@@ -81,29 +88,63 @@ class Ventana(object):
         self.frame_izquierda = ctk.CTkFrame(master=self.frame_principal, width=600, height=400, corner_radius=10)
         self.frame_izquierda.pack(side="left", padx=20, pady=30)
 
-        # Genero el Textarea
-        self.tk_textarea = tk.Text(self.frame_izquierda, height=26, width=70, background='white', highlightthickness=0)
-        self.tk_textarea.grid(row=0, column=0, sticky="nsew")
-        ctk_scroll = ctk.CTkScrollbar(self.frame_izquierda, command=self.tk_textarea.yview)
-        ctk_scroll.grid(row=0, column=1, sticky="ns")
+        # Pinto los paneles superior e inferior
+        self.generar_frame_izq_sup()
+        self.generar_frame_izq_inf()
+
+    def generar_frame_izq_sup(self):
+        # Frame superior
+        self.frame_superior = ctk.CTkFrame(master=self.frame_izquierda, width=600, height=200, corner_radius=10)
+        self.frame_superior.pack(side="top", padx=20, pady=10, fill="both", expand=True)
+
+        # Crear un frame para las barras de progreso
+        self.frame_barras = ctk.CTkFrame(master=self.frame_superior, fg_color="transparent")
+        self.frame_barras.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+        # Crear y configurar las barras de progreso
+        self.barras = []
+        self.valores = []
+        for i in range(5):
+            # Label para mostrar el valor numérico
+            valor = tk.Label(self.frame_barras, text="000", font=("Arial", 12, "bold"), width=3, anchor="center")
+            valor.grid(row=0, column=i, padx=18, pady=5, sticky="ew")
+            self.valores.append(valor)
+
+            # Barra de progreso vertical
+            barra = ttk.Progressbar(self.frame_barras, orient="vertical", length=200, mode="determinate", maximum=400)
+            barra.grid(row=1, column=i, padx=20, pady=5, sticky="nsew")
+            self.barras.append(barra)
+
+        # Crear el Textarea
+        self.tk_textarea = tk.Text(self.frame_superior, height=10, width=45, background='white',
+                                   highlightthickness=0)
+        self.tk_textarea.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+        ctk_scroll = ctk.CTkScrollbar(self.frame_superior, command=self.tk_textarea.yview)
+        ctk_scroll.grid(row=0, column=2, sticky="ns")
         self.tk_textarea.configure(yscrollcommand=ctk_scroll.set)
 
+    def generar_frame_izq_inf(self):
+        # Frame inferior
+        self.frame_inferior = ctk.CTkFrame(master=self.frame_izquierda, width=600, height=200, corner_radius=10)
+        self.frame_inferior.pack(side="top", padx=20, pady=10, fill="both", expand=True)
+
         # Genero un nuevo frame para colocar los botones
-        self.button_frame = ctk.CTkFrame(master=self.frame_izquierda,  fg_color="transparent")
-        self.button_frame.grid(row=1, column=0, columnspan=3, pady=20)
-        
+        self.button_frame = ctk.CTkFrame(master=self.frame_inferior, fg_color="transparent")
+        self.button_frame.grid(row=0, column=0, columnspan=3, pady=20)
+
         # Genero los botones / Grabar - Parar - Generar
-        self.btn_grabar = ctk.CTkButton(master=self.button_frame, text="Grabar", font=("Arial", 15, "bold"), width=120, height=45, command=self.funcion_grabar)
+        self.btn_grabar = ctk.CTkButton(master=self.button_frame, text="Grabar", font=("Arial", 15, "bold"), width=120,
+                                        height=45, command=self.funcion_grabar)
         self.btn_grabar.pack(side="left", padx=20)
 
-        # self.btn_parar = ctk.CTkButton(master=self.button_frame, text="Detener", font=("Arial", 15, "bold"), width=120, height=45, command=self.funcion_detener)
-        # self.btn_parar.pack(side="left", padx=20)
+        self.btn_parar = ctk.CTkButton(master=self.button_frame, text="Detener", font=("Arial", 15, "bold"), width=120, height=45, command=self.funcion_detener)
+        self.btn_parar.pack(side="left", padx=20)
 
-        self.btn_generar = ctk.CTkButton(master=self.button_frame, text="Generar", font=("Arial", 15, "bold"), width=120, height=45, command=self.funcion_generar)
+        self.btn_generar = ctk.CTkButton(master=self.button_frame, text="Generar", font=("Arial", 15, "bold"),
+                                         width=120, height=45, command=self.funcion_generar)
         self.btn_generar.pack(side="left", padx=20)
 
-        # self.btn_parar.configure(state="disabled")
-        # self.btn_generar.configure(state="disabled")
+
 
     def generar_frame_derecha(self):
          # ***************************************
@@ -144,45 +185,26 @@ class Ventana(object):
     def on_button_click(self, index):
         abecedario = "abcdefghijklmnñopqrstuvwxyz"
         print(f"Letra asignada: {abecedario[index]}")
+        self.letra = abecedario[index]
 
     def generar_frame3(self):
         # ************************************************
-        # ** CREO EL FRAME QUE VA A PERMITIR CONFIGURAR **
+        # ** CREO EL FRAME QUE VA A CONTENER LA LECTURA **
         # ************************************************
-        self.frame_config = ctk.CTkFrame(master=self.frame_footer, width=self.app.winfo_screenwidth()/2, height=80, corner_radius=10)
+        self.frame_config = ctk.CTkFrame(master=self.frame_footer, width=self.app.winfo_screenwidth()/2, height=80, corner_radius=30)
         self.frame_config.pack(padx=50, pady=0, expand=True, fill="both")
 
         # Genero el frame centrado
-        self.frame_config_center = ctk.CTkFrame(master=self.frame_config, fg_color="transparent")
-        self.frame_config_center.pack(expand=True)
-        self.frame_config_center.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        self.frame_config_center = ctk.CTkFrame(master=self.frame_config, fg_color="transparent", corner_radius=30)
+        self.frame_config_center.pack(fill="both", expand=True)
 
-        # Label  Puerto
-        etiqueta_puerto = ctk.CTkLabel(master=self.frame_config_center, text="puerto", corner_radius=8, font=("Arial", 15, "bold"))
-        etiqueta_puerto.pack(side="left", padx=20)
-        etiqueta_puerto.configure(text="PUERTO:")
+        # Crear el Textarea centrado dentro de self.frame_config_center
+        self.tk_textarea2 = tk.Text(self.frame_config_center, background='white', highlightthickness=0, height=5, width=50)
+        self.tk_textarea2.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-        # Genero el combo de los puertos activos
-        puertos = self.obtener_puertos_COM_activos()
-        self.combobox_puerto = ctk.CTkComboBox(master=self.frame_config_center, values=puertos, state="readonly")
-        self.combobox_puerto.pack(side="left", padx=20)
-
-        # Cargo la imagen que va a tener el boton buscar
-        imagen_path = os.path.join(os.path.dirname(__file__), "./icons/buscar.png")
-        imagen = ctk.CTkImage(light_image=Image.open(imagen_path), size=(20, 20) )
-
-        # Boton para recargar puertos activos
-        btn_buscar = ctk.CTkButton(master=self.frame_config_center, image=imagen, text="", font=("Arial", 15, "bold"), width=45, height=45, command=self.buscar_puertos)
-        btn_buscar.pack(side="left", padx=20)
-
-        # Label Baudios
-        etiqueta_baud = ctk.CTkLabel(master=self.frame_config_center, text="baud", corner_radius=8, font=("Arial", 15, "bold"))
-        etiqueta_baud.pack(side="left", padx=20)
-        etiqueta_baud.configure(text="BAUDIOS:")
-
-        # Genero el combo de los baudios
-        self.combobox_baud = ctk.CTkComboBox(master=self.frame_config_center, values=[str(value) for value in [300, 600, 750, 1200, 2400, 4800, 9600, 19200, 31250, 38400, 57600, 74880, 115200, 230400, 250000, 460800, 500000, 921600, 1000000, 2000000]], state="readonly")
-        self.combobox_baud.pack(side="left", padx=20)
+        # Configurar el peso de la fila y columna para que el Textarea se expanda correctamente
+        self.frame_config_center.grid_rowconfigure(0, weight=1)
+        self.frame_config_center.grid_columnconfigure(0, weight=1)
 
     def buscar_puertos(self):
         # Metodo par repintar el combo con los nuevos puertos activos
@@ -202,7 +224,8 @@ class Ventana(object):
         # ** CREO EL FRAME QUE VA A PERMITIR EXPORTAR **
         # **********************************************
         
-        self.frame_export = ctk.CTkFrame(master=self.frame_footer, width=self.app.winfo_screenwidth()/2, height=80, corner_radius=10)
+        self.frame_export = ctk.CTkFrame(master=self.frame_footer, width=self.app.winfo_screenwidth()/2, height=80,
+                                         corner_radius=10)
         self.frame_export.pack(padx=50, pady=20, expand=True, fill="both")
         
         # Genero el frame centrado
@@ -210,26 +233,44 @@ class Ventana(object):
         self.frame_export_center.pack(expand=True)
         self.frame_export_center.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-        # Genero el label
-        etiqueta = ctk.CTkLabel(master=self.frame_export_center, text="Exportar a:", corner_radius=8, font=("Arial", 15, "bold"))
-        etiqueta.pack(side="left", padx=20)
-        etiqueta.configure(text="EXPORTAR A:")
+        # Label  Puerto
+        etiqueta_puerto = ctk.CTkLabel(master=self.frame_export_center, text="puerto", corner_radius=8,
+                                       font=("Arial", 15, "bold"))
+        etiqueta_puerto.pack(side="left", padx=20)
+        etiqueta_puerto.configure(text="PUERTO:")
 
-        # Variable para el radio button
-        self.radio_var = tk.StringVar(value="CSV")
+        # Genero el combo de los puertos activos
+        puertos = self.obtener_puertos_COM_activos()
+        self.combobox_puerto = ctk.CTkComboBox(master=self.frame_export_center, values=puertos, state="readonly")
+        self.combobox_puerto.pack(side="left", padx=20)
 
-        # Genero el RadioButton
-        self.radio_csv = ctk.CTkRadioButton(
-            master=self.frame_export_center,
-            text="CSV",
-            variable=self.radio_var,
-            value="CSV"
-        )
-        self.radio_csv.place(relx = 0.55, rely = 0.4, anchor=tk.CENTER)
-        self.radio_csv.pack(side="left", padx=20)
+        # Cargo la imagen que va a tener el boton buscar
+        imagen_path = os.path.join(os.path.dirname(__file__), "./icons/buscar.png")
+        imagen = ctk.CTkImage(light_image=Image.open(imagen_path), size=(20, 20))
+
+        # Boton para recargar puertos activos
+        btn_buscar = ctk.CTkButton(master=self.frame_export_center, image=imagen, text="", font=("Arial", 15, "bold"),
+                                   width=45, height=45, command=self.buscar_puertos)
+        btn_buscar.pack(side="left", padx=20)
+
+        # Label Baudios
+        etiqueta_baud = ctk.CTkLabel(master=self.frame_export_center, text="baud", corner_radius=8,
+                                     font=("Arial", 15, "bold"))
+        etiqueta_baud.pack(side="left", padx=20)
+        etiqueta_baud.configure(text="BAUDIOS:")
+
+        # Genero el combo de los baudios
+        self.combobox_baud = ctk.CTkComboBox(master=self.frame_export_center, values=[str(value) for value in
+                                                                                      [300, 600, 750, 1200, 2400, 4800,
+                                                                                       9600, 19200, 31250, 38400, 57600,
+                                                                                       74880, 115200, 230400, 250000,
+                                                                                       460800, 500000, 921600, 1000000,
+                                                                                       2000000]], state="readonly")
+        self.combobox_baud.pack(side="left", padx=20)
 
         # Genero boton exportar
-        self.btn_export = ctk.CTkButton(master=self.frame_export_center, text="Exportar", font=("Arial", 15, "bold"), width=120, height=45, command=self.funcion_exportar)
+        self.btn_export = ctk.CTkButton(master=self.frame_export_center, text="Exportar", font=("Arial", 15, "bold"),
+                                        width=120, height=45, command=self.funcion_exportar)
         self.btn_export.pack(side="left", padx=20)
         self.btn_export.configure(state="disabled")
         
@@ -255,51 +296,109 @@ class Ventana(object):
             print("Valor seleccionado:", baud)
 
             self.btn_grabar.configure(state="disabled")
-            # self.btn_parar.configure(state="normal")
+            self.btn_parar.configure(state="normal")
             self.btn_generar.configure(state="disabled")
-        
+
             ## Comienzo a leer puerto serie
-            self.detener_lectura = False
-            self.serial_reader = serial.Serial(port=puerto, baudrate=baud)
-            self.hilo_lectura = threading.Thread(target=self.leer_puerto_serie, args=(self.serial_reader,))
-            self.hilo_lectura.start()
+            try:
+                self.serial_reader = serial.Serial(port=puerto, baudrate=baud)
+                self.leer_puerto_serie(self.serial_reader)
+                self.btn_grabar.configure(state="disabled")
+                self.btn_generar.configure(state="normal")
+            except serial.SerialException as e:
+                print(f"Error al abrir el puerto serie: {e}")
+
 
     def leer_puerto_serie(self, serial_reader = None):
-        while not self.detener_lectura:
-            if serial_reader.in_waiting > 0:
+        datos = [[] for _ in range(5)]
+        acelera = []
+        for _ in range(6):
+            try:
                 texto_recibido = serial_reader.readline().decode('utf-8', errors='ignore').strip()
-                print(texto_recibido)
-                self.tk_textarea.insert(tk.END, texto_recibido + '\n')
-                self.tk_textarea.see(tk.END)     
+                valores = texto_recibido.split(';')
+                for i in range(5):
+                    datos[i].append(float(valores[i]))
+                acelera.append(valores[5])
+            except Exception as e:
+                print(f"Error leyendo el puerto serie: {e}")
+        print(datos)
+        print(acelera)
+        self.funcion_detener()
+        self.mediaDedos = [sum(d) / len(d) if d else 0 for d in datos]
+        data_matrix = [list(map(float, row.split(','))) for row in acelera]
+        transposed_data = list(zip(*data_matrix))
+        self.accelerometro = [sum(column) for column in transposed_data]
+        self.actualizar_barras_progreso()
+        self.actualizar_textarea()
+        print("Lecturas dedos: ")
+        print(self.mediaDedos)
+        print("Lecturas accelerometro: ")
+        print(self.accelerometro)
+
+    def actualizar_barras_progreso(self):
+        print("Actualizando barras de progreso con los datos leidos...")
+        for i, media in enumerate(self.mediaDedos):
+            self.barras[i].config(value=media)
+            self.valores[i].config(text=str(media))
+
+    def actualizar_textarea(self):
+        for dato in self.accelerometro:
+            self.tk_textarea.insert(tk.END, f"{dato}\n")
 
     def funcion_detener(self):
-        self.detener_lectura = True
-        if self.hilo_lectura and self.hilo_lectura.is_alive():
-            self.hilo_lectura.join() 
-        self.serial_reader.close()
         print("Función detener")
+        if self.serial_reader.is_open:
+            self.serial_reader.close()
+
         self.btn_grabar.configure(state="normal")
         if self.tk_textarea.get("1.0", "end-1c"):
             self.btn_generar.configure(state="normal")
-        
+
 
     def funcion_generar(self):
-        print("Función guardar")
-        contenido = self.tk_textarea.get("1.0", tk.END)  
-        filas = contenido.strip().split('\n')
-        datos = [list(map(float, fila.split(';'))) for fila in filas]
+        print("Función generar")
+        if len(self.mediaDedos) > 0 and len(self.accelerometro) > 0 and self.letra != "":
+            self.tk_textarea2.delete('1.0', tk.END)
+            dedos = ""
+            for i, dato in enumerate(self.mediaDedos):
+                print(str(round(dato, 2)))
+                dedos = dedos + str(round(dato, 2))+","
+            accelerometro = ""
+            for i, dato in enumerate(self.accelerometro):
+                print(str(round(dato, 2)))
+                accelerometro = accelerometro + str(round(dato, 2))+";"
+            self.tk_textarea2.insert(tk.END, f"{self.letra},{dedos}{accelerometro[:-1]}\n")
+        else:
+            err = ""
+            if self.letra == "":
+                err = err + "La letra"
+            if len(self.mediaDedos) == 0:
+                if err != "":
+                    err = err + ", las lecturas de los dedos"
+                else:
+                    err = err + "Las lecturas de los dedos"
+            if len(self.accelerometro) == 0:
+                if err != "":
+                    err = err + " y las lecturas del accelerometro"
+                else:
+                    err = err + "Las lecturas del accelerometro"
+            self.mostrar_mensaje("Error", "Faltan los datos: " + err)
+
         self.btn_export.configure(state="normal")
 
+        # contenido = self.tk_textarea.get("1.0", tk.END)
+        # filas = contenido.strip().split('\n')
+        # datos = [list(map(float, fila.split(';'))) for fila in filas]
         # Convertir los datos a un arreglo de NumPy
-        datos_np = np.array(datos)
-        self.grafica_fourier.datos_procesados = datos_np
-        self.grafica_fourier.pintar_grafica(self.frame_derecha)
+        # datos_np = np.array(datos)
+        # self.grafica_fourier.datos_procesados = datos_np
+        # self.grafica_fourier.pintar_grafica(self.frame_derecha)
 
     """Obtiene los valores de los checkboxes seleccionados."""
     def funcion_exportar(self):
         print("Exportando datos")
-        if self.radio_var.get() == "CSV":
-            self.grafica_fourier.exportar_txt("CSV")
+        with open(f"{self.letra}.csv", 'w') as f:
+            f.write(f"{self.tk_textarea2.get("1.0", "end-1c")}\n")
 
     
     def mostrar_mensaje(self, msgTipe, msgContent):
